d0776f3daac9be5913f9156845b0355958f62477
kohdice <kohdice@gmail.com>
2023-05-15 13:06:19 +0000

    Add function upload media to S3

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "maintenance",
+    "storage",
     "django_extensions",
     "import_export",
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+import boto3
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from config import settings
+
+
+class IndexView(TemplateView):
+    template_name = "storage/loader.html"
+
+
+index = IndexView.as_view()
+
+
+def upload_file(request):
+    s3 = boto3.resource(
+        service_name="s3",
+        endpoint_url=os.environ["ENDPOINT"],
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name="",
+    )
+
+    bucket = s3.Bucket(os.environ["S3_BUCKET_NAME"])
+
+    file_name = "test.png"
+    file_path = f"{settings.BASE_DIR}/{file_name}"
+
+    bucket.upload_file(file_path, file_name)
+    return HttpResponse("Upload Complete")
